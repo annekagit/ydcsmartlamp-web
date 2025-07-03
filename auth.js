5d40f1131ee1e249785cf306cfe0e267b014ffc3
annekagit <annekaparfum@gmail.com>
2025-07-03 18:00:45 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,7 @@
-firebase.initializeApp(firebaseConfig);
-firebase.auth().onAuthStateChanged(user => {
-  if (!user && location.pathname !== "/login.html") {
-    location.href = "login.html";
+// auth.js
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!user) {
+    // Redirect ke login jika belum login
+    window.location.href = "login.html";
   }
-});
-
-document.getElementById("login-form")?.addEventListener("submit", e => {
-  e.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => location.href = "index.html")
-    .catch(err => alert("Login gagal: " + err.message));
 });
