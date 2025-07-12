af46c4cf358136e8abccab2ac3098fdf6a386da3
annekagit <annekaparfum@gmail.com>
2025-07-12 20:07:39 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDNa6c6E4G9vVAJC3K_uSmwzrz_rKQ2L9Q",
+  authDomain: "smartlamp-2025-1a143.firebaseapp.com",
+  databaseURL: "https://smartlamp-2025-1a143-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smartlamp-2025-1a143",
+  storageBucket: "smartlamp-2025-1a143.firebasestorage.app",
+  messagingSenderId: "322116293631",
+  appId: "1:322116293631:web:2e5534e5f4b03012424dca"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
